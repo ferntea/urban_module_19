f6bb845afd0a9e6da2cea4c259534d07025bdc43
ferntea <58177994+ferntea@users.noreply.github.com>
2024-10-16 00:20:38 +0300

    task 1

---
+++ UrbanDjango_m19/task1/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)  # Username of the buyer
+    balance = models.DecimalField(max_digits=10, decimal_places=2)  # Balance
+    age = models.PositiveIntegerField()  # Age of the buyer
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)  # Title of the game
+    cost = models.DecimalField(max_digits=10, decimal_places=2)  # Cost of the game
+    size = models.DecimalField(max_digits=10, decimal_places=2)  # Size of the game files
+    description = models.TextField()  # Description of the game
+    age_limited = models.BooleanField(default=False)  # Age restriction
+    buyers = models.ManyToManyField(Buyer, related_name='games')  # Buyers who own the game
+
+    def __str__(self):
+        return self.title
