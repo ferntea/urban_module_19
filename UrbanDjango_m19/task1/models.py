be0531ab5d1917ef9c00c629011f76ade0669571
ferntea <58177994+ferntea@users.noreply.github.com>
2024-10-16 18:56:09 +0300

    task 1 query set

--- UrbanDjango_m19/task1/models.py
+++ UrbanDjango_m19/task1/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-
+from task1.models import Buyer
 class Buyer(models.Model):
     name = models.CharField(max_length=100)  # Username of the buyer
     balance = models.DecimalField(max_digits=10, decimal_places=2)  # Balance
