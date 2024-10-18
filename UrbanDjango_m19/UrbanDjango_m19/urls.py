4c4d3ae14c0c5d18c3a4a33a3fed14ad759ca060
ferntea <58177994+ferntea@users.noreply.github.com>
2024-10-18 15:17:34 +0300

    task 1 + MVT_1

--- UrbanDjango_m19/UrbanDjango_m19/urls.py
+++ UrbanDjango_m19/UrbanDjango_m19/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from task1.views import sign_up_by_html, home, shop_view, cart_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('home/', home, name='home'),
+    path('shop/', shop_view, name='shop'),
+    path('cart/', cart_view, name='cart'),
+    path('register/', sign_up_by_html, name='register'),
 ]
--- UrbanDjango_m19/task1/views.py
+++ UrbanDjango_m19/task1/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserRegister
 
-# Create your views here.
+# Sample list of existing users (consider using a database in a real application)
+users = ['Ann', 'Anton', 'Peter']
+
+template_name = 'first_task/registration_page.html'
+
+def sign_up_by_html(request):
+    form = UserRegister()
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            password_repeat = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+            if username in users:
+                form.add_error('username', 'Пользователь уже существует')
+            elif password != password_repeat:
+                form.add_error('repeat_password', 'Пароли не совпадают')
+            elif age < 18:
+                form.add_error('age', 'Вы должны быть старше 18')
+            else:
+                # Here you can add the user to your database
+                # For example: User.objects.create_user(username=username, email=email, password=password)
+                return redirect('home')  # Redirect to home after successful registration
+
+    return render(request, template_name, {'form': form})
+
+def home(request):
+    return render(request, 'first_task/home.html')
+
+def shop_view(request):
+    return render(request, 'first_task/shop.html')
+
+def cart_view(request):
+    return render(request, 'first_task/cart.html')
